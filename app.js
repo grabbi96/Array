f645374e5a5716c58cdbed02b94266bc2572566d
grabbi <grabbi96@gmail.com>
2018-07-08 08:41:37 -0700

    Array

---
+++ app.js
@@ -0,0 +1,20 @@
+var arr = [null, -7, 63, -25, true, 20, 0, "", 97, "Rijwan"];
+
+var resultArr = arr.map(function(element) {
+  if (typeof element === "number") {
+    if (element < 0) {
+      return element * 10;
+    } else {
+      return element;
+    }
+  } else if (element === undefined || element === null || element === "") {
+    element = 1;
+    return element;
+  } else if (element === true) {
+    element = undefined;
+    return element;
+  } else {
+    return element;
+  }
+});
+console.log(resultArr);
