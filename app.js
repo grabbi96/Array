d6c80669c4e9239bfbe2b7511c428a30b96349f9
grabbi <grabbi96@gmail.com>
2018-07-08 10:55:06 -0700

    improve

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 var arr = [null, -7, 63, -25, true, 20, 0, "", 97, "Rijwan"];
 
-var resultArr = arr.map(function (element) {
-    if (typeof element === "number") {
-        if (element < 0) {
-            if (element % 5 === 0) {
-                return element * 10
-            } else {
-                return element
-            }
-        } else {
-            return element;
-        }
-    } else if (element === undefined || element === null || element === "") {
-        element = 1;
-        return element;
-    } else if (element === true) {
-        element = undefined;
-        return element;
+var resultArr = arr.map(function(element) {
+  if (typeof element === "number") {
+    if (element < 0 && element % 5 === 0) {
+      return element * 10;
     } else {
-        return element;
+      return element;
     }
+  } else if (
+    element === undefined ||
+    element === null ||
+    element === "" ||
+    element === false
+  ) {
+    element = 1;
+    return element;
+  } else if (element === true) {
+    element = undefined;
+    return element;
+  } else {
+    return element;
+  }
 });
 console.log(resultArr);
